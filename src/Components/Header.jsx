05c9c6844dde1c059abd1b9cb1b4710c555254ce
Tejasp2003 <tejasp200320@gmail.com>
2023-04-17 12:05:16 +0530

    Added Dark Mode functionality

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import MenuItem from './MenuItem'
 import {AiFillHome} from 'react-icons/ai'
 import {BsFillInfoCircleFill} from 'react-icons/bs'
+import DarkModeSwitch from "./DarkModeSwitch"
 import Link from 'next/link'
 
 const Header = () => {
@@ -11,7 +12,8 @@ const Header = () => {
         <MenuItem title= "HOME" address= "/" Icon= {AiFillHome} />
         <MenuItem title= "ABOUT" address= "/about" Icon= {BsFillInfoCircleFill} />
       </div>
-      <div className='sm:mr-2'>
+      <div className='sm:mr-2 flex items-center space-x-5'>
+        <DarkModeSwitch />
         <Link href="/">
           <h2 className='text-2xl'>
             <span className='mr-1 font-bold bg-amber-500 py-1 px-2 rounded-lg'>IMDb</span>
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,19 +1,21 @@
-import './globals.css'
-import Header from '@/Components/Header'
-
+import "./globals.css";
+import Header from "@/Components/Header";
+import Providers from "./Providers";
 export const metadata = {
-  title: 'IMDB Clone',
-  description: 'IMDB Clone suing NextJs and Tailwind',
-}
+  title: "IMDB Clone",
+  description: "IMDB Clone suing NextJs and Tailwind",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      
       <body>
-      <Header/>
-        
-        {children}</body>
+        <Providers>
+          <Header />
+
+          {children}
+        </Providers>
+      </body>
     </html>
-  )
+  );
 }
