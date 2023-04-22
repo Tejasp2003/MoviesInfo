4e872e5f59e0129200096da132c09ca20705d13f
Tejasp2003 <tejasp200320@gmail.com>
2023-04-22 09:24:05 +0530

    fixed deployment issue

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import {FiThumbsUp} from "react-icons/fi"
+// import {AiOutlineHeart} from "react-icons/ai"
 
 const Card = ({ result }) => {
   return (
@@ -36,6 +37,7 @@ const Card = ({ result }) => {
               }
               <FiThumbsUp className="h-5 mr-1 ml-3" />{result.vote_count}
               </p>
+           
         </div>
       </Link>
     </div>
